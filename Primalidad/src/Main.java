23309ae03327f6faa2f76c8e36470f416cacb44b
Sebastian Oteiza <oteiza.sebastian@gmail.com>
2022-12-14 21:31:55 -0300

    Ejemplo Primalidad en NetBeans

---
+++ Primalidad/src/Main.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+//APLICACION PARA DETECTAR SI EL NUMERO INGRESADO ES PRIMO 
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+        int numero = leer.nextInt();
+        
+        if(esPrimo(numero)){
+            System.out.println("=================");
+            System.out.println("Es numero primo");
+            System.out.println("=================");
+        }else{
+            System.out.println("=================");
+            System.out.println("No es numero primo");
+            System.out.println("=================");
+        }
+    }
+    
+    static boolean esPrimo(int numero){
+        int contador = 0; //CONTADOR, CON CUANTOS NUMEROS SE PUEDE DIVIDIR EL NUMERO INGRESADO, VALOR INICIAL CERO
+        
+        int [] numeros = new int [numero];
+        for(int i = 0; i < numeros.length; i++){
+            numeros[i] = i+1;
+            
+        }
+        for(int i : numeros){
+            if(i == 1 || i == numero){
+                continue;
+            }
+            if(numero % i == 0){
+                contador++;
+            }
+        }
+        return contador == 0;
+    }
+}
