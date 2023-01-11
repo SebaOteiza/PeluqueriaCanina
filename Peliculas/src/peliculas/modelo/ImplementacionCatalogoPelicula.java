edf2cbd9ddcc245469a659f4eb878258a648ae9f
Sebastian Oteiza <oteiza.sebastian@gmail.com>
2023-01-11 18:06:55 -0300

    Implementación de catalogo peliculas (interface)

---
+++ Peliculas/src/peliculas/modelo/ImplementacionCatalogoPelicula.java
@@ -0,0 +1,64 @@
+
+package peliculas.modelo;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+
+public class ImplementacionCatalogoPelicula implements ICatalogoPeliculas{
+    
+    private final ArrayList<String> listaPeliculas;
+
+    @Override
+    public void insertarPelicula(Pelicula nombrePelicula) {
+        this.listaPeliculas.add(nombrePelicula.toString());
+        
+    }
+    
+    //constructor
+    public ImplementacionCatalogoPelicula() {
+        this.listaPeliculas = new ArrayList<>();
+    }
+
+    @Override
+    public void listarPelicula() {
+        JOptionPane.showMessageDialog(
+                null, 
+                this.listaPeliculas,
+                "LISTA DE PELICULAS",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    @Override
+    public void buscarPelicula(String nombrePelicula) {
+        String resultado = null;
+        for(var pelicula: this.listaPeliculas){
+            if(pelicula.equals(nombrePelicula)){
+                resultado = pelicula;
+                break;
+            }else{
+                resultado = pelicula;
+            }
+        }
+        
+        if(nombrePelicula.equals(resultado)){
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Resultado "+ resultado, 
+                    "BUSCAR PELICULA", 
+                    JOptionPane.QUESTION_MESSAGE
+            );
+                
+            
+        }else{
+            JOptionPane.showMessageDialog(
+                    null, 
+                    "No se encontró la Película" + nombrePelicula, 
+                    "BUSCAR PELÍCULA", 
+                    JOptionPane.WARNING_MESSAGE);
+        }
+        
+        
+    }
+
+}
