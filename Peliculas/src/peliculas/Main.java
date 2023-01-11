79c5d465c2ebfb8738810ff094038da8858619a6
Sebastian Oteiza <oteiza.sebastian@gmail.com>
2023-01-11 17:30:51 -0300

    Creación ejemplo  de ARRAYLIST

--- Peliculas/src/peliculas/Main.java
+++ Peliculas/src/peliculas/Main.java
@@ -1,11 +1,27 @@
 
 package peliculas;
 
-//JAVA BEANS 
+import java.util.ArrayList; //el arrayList almacena objetos 
 public class Main {
 
    
     public static void main(String[] args) {
+        
+        ArrayList<String> meses = new ArrayList<>(); //así se define un arraylist
+        
+        //no definimos la cantidad que se van a almacenar a diferencia del ARRAY 
+        
+        meses.add("Enero"); //el add es para asignar un nuevo dato
+        meses.add("Febrero");
+        meses.add("Marzo");
+        meses.add("Abril");
+        System.out.println(meses);
+        
+        //también se puede iterar con iun foreach
+        
+        for(String dato : meses){
+            System.out.println(dato);
+        }
     }
     
 }
