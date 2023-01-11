825d902b725c7f8e6b49b6594a12f9f277bc86bf
Sebastian Oteiza <oteiza.sebastian@gmail.com>
2023-01-11 17:46:18 -0300

    Creación de ejemplos de cuadros de diálogo con InputDialog y ShowMessage

--- Peliculas/src/peliculas/Main.java
+++ Peliculas/src/peliculas/Main.java
@@ -2,12 +2,54 @@
 package peliculas;
 
 import java.util.ArrayList; //el arrayList almacena objetos 
+import javax.swing.JOptionPane;
 public class Main {
 
    
     public static void main(String[] args) {
         
-        ArrayList<String> meses = new ArrayList<>(); //así se define un arraylist
+        /*
+        JOptionPane.ERROR_MESSAGE. -> 0
+        JOptionPane.INFORMATION:MESSAGE. -> 1
+        JOptionPane.WARNING_MESSAGE. -> 2
+        JOptionPane.QUESTION_MESSAGE. -> 3
+     
+        */
+        
+        
+        //TODO LO QUE INGRESA POR EL CUADRO DE DIALOGO SERÁ CONSIDERADO STRING 
+        
+        String valor = JOptionPane.showInputDialog(
+                null,
+                "Ingrese un texto",
+                "Entrada",
+                3);
+        
+        //me va a imprimir un cuadro de diálogo
+        JOptionPane.showMessageDialog(
+                null, 
+                valor, //ESTE VALOR EQUIVALE A LO QUE SE INGRESÓ ARRIBA EN EL INPUT CREADO 
+                "Mi Mensaje",
+               JOptionPane.ERROR_MESSAGE );
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*ArrayList<String> meses = new ArrayList<>(); //así se define un arraylist
         
         //no definimos la cantidad que se van a almacenar a diferencia del ARRAY 
         
@@ -21,7 +63,7 @@ public class Main {
         
         for(String dato : meses){
             System.out.println(dato);
-        }
+        }*/
     }
     
 }
